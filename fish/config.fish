00905e5e83791a8c56fe18d71ee10f8286eb58c5
emmdie <emmanuel.diehl@ifsr.de>
2026-06-05 20:13:27 +0200

    make pretty fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,32 @@
+function fish_greeting
+    set_color yellow
+    printf "  %s " (date +%H:%M)
+
+    set_color brblack
+    printf "│ "
+    set_color cyan
+    printf "󰍛 %s " (free -h | awk '/^Mem:/ {print $3 "/" $2}')
+
+    set_color brblack
+    printf "│ "
+    set_color green
+    printf "󰘚 %s%% " (top -bn1 | awk '/Cpu\(s\)/ {print int($2+$4)}')
+
+    set_color brblack
+    printf "│ "
+    set_color blue
+    printf "󰋊 %s " (df -h / | awk 'NR==2 {print $5}')
+
+    set_color brblack
+    printf "│ "
+    set_color magenta
+    printf "Pacman: 󰚰 %s " (checkupdates 2>/dev/null | count)
+
+    printf "\n"
+
+    set_color normal
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
