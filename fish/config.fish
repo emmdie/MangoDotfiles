cf19b6ceabf36f004d743663d6d2f61b75699377
emmdie <emmanuel.diehl@ifsr.de>
2026-06-05 19:18:35 +0200

    make fish enforce virtual python environments

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+# This prevents me from installing packages with pip without being in a virtualenv first.
+set -g -x PIP_REQUIRE_VIRTUALENV true
+
+# ABBREVIATIONS
 abbr --add hx helix
